6978623cced0ce084c5fe8ab6eee3f1f2ed349a9
Aastha 204 <aasthagarg333@gmail.com>
2024-01-30 10:26:13 +0530

    Add github link

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["College Student", "Coder"],
+    strings: ["Data Scientist", "AI Enthusiast"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
